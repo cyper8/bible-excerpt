ab2890a607b163cfd852b41362cde8fdba74f924
cyper8 <anton.yashchuk@gmail.com>
2025-02-20 17:48:47 +0200

    fixed data alignment in calendar

--- src/bible-reading-calendar.ts
+++ src/bible-reading-calendar.ts
@@ -1,4 +1,4 @@
-import { LitElement, PropertyValueMap, css, html } from "lit";
+import { LitElement, PropertyValueMap, css, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 
 export declare interface ReadingData {
@@ -16,6 +16,47 @@ export const daysInMonth = (m0: number, y?: number) => {
   d.setMonth(m0+1);
   d.setDate(0);
   return d.getDate()
+}
+
+const genMonth = (data:string[][], currentReadingDate: Date) => {
+  const week = [
+    "Пн",
+    "Вт",
+    "Ср",
+    "Чт",
+    "Пт",
+    "Сб",
+    "Нд",
+  ];
+  let month = currentReadingDate.getMonth();
+  let year = currentReadingDate.getFullYear();
+  let day1 = new Date(year, month, 1); 
+  let offset = day1.getDay();
+  let length = daysInMonth(month, year);
+  //let mdata = Array<string>(offset).fill('').concat(...data[month]);
+  let gen: (ReadingData | undefined)[][] = [];
+  for (var w=0;w<Math.ceil((length+offset-1)/7); w++) {
+    gen[w]=[];
+    for (var d=0;d<7;d++) {
+      let id = (w*7+d)-offset+1;
+      gen[w][d]= (id>=0 && data[month] && data[month][id]) ? {
+        date: new Date(year, month, id+1),
+        reading: data[month][id]
+      } : undefined;
+    }
+  }
+  
+  return html`<table>
+  <thead><tr>${week.map(d => html`<th>${d}</th>`)}</tr></thead>
+  <tbody>
+    ${gen.map((w,wn) => html`<tr class="week" id="week${wn}">${
+      w.map((d,dn) => {
+        let date = (wn*7+dn+1)-(offset-1)
+        return html`<td class="day" id="day${dn}" alt="${d?.reading}">${date>0?date+'':''}</td>`
+      })
+    }</tr>`)}
+  </tbody>
+  </table>`
 }
 
 @customElement('bible-reading-calendar')
@@ -57,10 +98,10 @@ export class BibleReadingCalendar extends LitElement {
             } else return ''
           })
           .then(rData => {
-            readingData[month][day] = rData;
+            readingData[month][day-1] = rData;
             if (rData && !reportedFlag) {
               reportedFlag = true;
-              this.currentReadingDate = new Date(year, month-1, day);
+              this.currentReadingDate = new Date(year, month, day);
               this.dispatchEvent(new CustomEvent<ReadingData>('reading-date-selected', {
                 detail: {
                   date: this.currentReadingDate,
@@ -89,12 +130,12 @@ export class BibleReadingCalendar extends LitElement {
   protected render() {
     return html`
     <label class="icon" id="clock" for="date-selector-switch">
-      ${this.date.toLocaleDateString(
+      ${(this.currentReadingDate || this.date).toLocaleDateString(
         navigator.language,
         {dateStyle: 'long'}
       )}<input type=checkbox id="date-selector-switch" hidden />
       <div class="date-selector">
-        ${this.monthReading}
+        ${this.currentReadingDate ? genMonth(this.monthReading, this.currentReadingDate): nothing}
       </div>
     </label>
     
@@ -146,9 +187,10 @@ export class BibleReadingCalendar extends LitElement {
     .date-selector {
       display: none;
       position: absolute;
-      width: 7em;
-      height: 9em;
+      width: auto;
+      height: auto;
       background-color: #eee;
+      color: #242424;
       border-radius: 1em;
     }
     input#date-selector-switch:checked+.date-selector {
